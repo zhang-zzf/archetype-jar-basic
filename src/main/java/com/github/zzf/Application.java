5fa8cab804741674a5bff69a5b54a1c75b86565c
zhanfeng.zhang <zhang.zzf@alibaba-inc.com>
2023-04-18 22:25:31 +0800

    feat: add spring-boot

--- src/main/java/com/github/zzf/Application.java
+++ src/main/java/com/github/zzf/Application.java
@@ -1,15 +1,19 @@
 package com.github.zzf;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author zhang.zzf
  * @date 2020-04-18
  */
 @Slf4j
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
         log.info("Hello,World");
     }
 
--- src/test/java/com/github/zzf/ApplicationTest.java
+++ src/test/java/com/github/zzf/ApplicationTest.java
@@ -2,10 +2,12 @@ package com.github.zzf;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
 @Slf4j
+@SpringBootTest
 public class ApplicationTest {
 
     @Test
